1dcf4f5721aaeca01333788e83df504a8931f250
Jimmy_wei <asasin1859@gmail.com>
2023-12-03 23:42:40 +0800

    struce register and print function finish

---
+++ gc.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "gc.h"
+
+char *data_type_str[] ={"CHAR","INT32","UINT8","UINT32","FLOAT","DOUBLE","OBJ_PTR","OBJ_STRUCT"};
+
+struct_db_t *struct_db = NULL;
+void print_struct_info (struct_info_t *struct_info);
+void print_struct_db();
+
+int struct_database_add(struct_info_t *struct_info)
+{
+	if(!struct_db) {
+		struct_db = (struct_db_t*) calloc(1,sizeof(struct_db_t));
+		if(!struct_db) return 1;
+	}
+
+	if(!struct_db->head) {
+		struct_db->head = struct_info;
+		struct_db->count = 1;
+	}
+	else
+	{
+		struct_info->next = struct_db->head;
+		struct_db->head = struct_info;
+		struct_db->count++;
+	}
+	return 0;
+}
+
+int struct_register(char *struct_name,unsigned int struct_size,unsigned int field_num,field_info_t *fields_array) {
+	struct_info_t *info = (struct_info_t*) calloc(1,sizeof(struct_info_t));
+	if(!info) {
+		printf("memory allocation failed\n");
+		return 1;
+	}
+	info->next = NULL;
+	strncpy(info->name,struct_name,MAX_STRUCTURE_NAME_LENGTH);
+	info->size = struct_size;
+	info->fields_num = field_num;
+	info->fields = fields_array;
+	return struct_database_add(info);
+}
+
+void print_struct_info(struct_info_t *struct_info)
+{
+	if(!struct_info) {
+		printf("This structure is nothing to print\n");
+		return;
+	}
+	
+	printf("|----------------------------------------------|\n"); //40
+	printf("|%-10s| size = %-8d| fields = %-8d|\n",struct_info->name,struct_info->size,struct_info->fields_num);
+	printf("|----------------------------------------------|\n"); //40
+	field_info_t *field = struct_info->fields;
+	int fieldnum;
+	for(fieldnum=0;fieldnum<struct_info->fields_num;fieldnum++)
+	{
+		printf("%-11s|%-3d%-15s| ","",fieldnum,field->name);
+		printf("data type = %-8s| ",data_type_str[fieldnum,field->dtype]);
+		printf("size = %-8d| ",field->size);
+		printf("offset = %-8d| ",field->offset);
+		printf("nested struct = %-15s|\n",field->nested_struct);
+		printf("%-11s|-------------------------------------------------------------------------------------------------------------|\n","");
+		
+		field++;
+	}
+	printf("\n");
+}
+
+void print_struct_db() {
+	if(!struct_db) return;
+	printf("print structure database\n");
+	printf("Number of registered structure is %d\n",struct_db->count);
+	
+	int currentcount = 1;
+	struct_info_t *node = struct_db->head;
+	while(node) {
+		printf("Structure no = %d (%p)\n",currentcount++,node);
+		print_struct_info(node);
+		node = node->next;
+	}
+	
+}
---
+++ gc.h
@@ -0,0 +1,47 @@
+#define MAX_STRUCTURE_NAME_LENGTH 128
+#define MAX_FIELD_NAME_LENGTH 128
+
+#define OFFSETOF(struct_name, field_name) (unsigned int)&(((struct_name *)0)->field_name)
+#define FIELD_SIZE(struct_name, field_name) sizeof(((struct_name *)0)->field_name)
+#define STRUCT_SIZE(struct_name) sizeof(#struct_name)
+
+typedef enum {
+	CHAR,
+	INT32,
+	UINT8,
+	UINT32,
+	FLOAT,
+	DOUBLE,
+	OBJ_PTR,
+	OBJ_STRUCT
+} data_type_t;
+
+typedef struct _field_info_{
+	char name[MAX_FIELD_NAME_LENGTH];
+	data_type_t dtype;
+	unsigned int size;
+	unsigned int offset;
+	char nested_struct[MAX_STRUCTURE_NAME_LENGTH];
+} field_info_t;
+
+typedef struct _struct_info_t{
+	struct _struct_info_t *next;
+	char name[MAX_STRUCTURE_NAME_LENGTH];
+	unsigned int size;
+	unsigned int fields_num;
+	field_info_t *fields;
+}struct_info_t;
+
+typedef struct _struct_db_{
+    struct_info_t *head;
+    unsigned int count;
+} struct_db_t;
+
+//abstract
+void print_struct_info (struct_info_t *struct_info);
+void print_struct_db();
+int struct_register(char *struct_name,unsigned int struct_size,unsigned int field_num,field_info_t *fields_array);
+
+//register marco
+#define FIELD_EXTEND(struct_name, fld_name, dtype, nested_struct) {#fld_name,dtype,FIELD_SIZE(struct_name,fld_name),OFFSETOF(struct_name,fld_name),#nested_struct}
+#define STRUCT_REGISTER(struct_name,fields_array) struct_register(#struct_name,(unsigned int)sizeof(struct_name),(unsigned int)sizeof(fields_array)/sizeof(field_info_t),fields_array)
